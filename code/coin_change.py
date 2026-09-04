e03ca84cff37bbc414b2dc1c804d3e9b9ac049a8
zillorz <12345anandkaran@gmail.com>
2026-09-04 10:09:02 -0400

    O(N) + greedy

--- code/coin_change.py
+++ code/coin_change.py
@@ -1,19 +1,18 @@
 def calculate(value: int, denominations: list[int]):
-    dp = [-1] * (value + 1) # make a list with indexes 0 to value, all 0
-    dp[0] = 0
+    dp = [-1] * (value + 1) # make a list with indexes 0 to value
+    dp[0] = 0 # 0 coins needed to make 0
 
     for v in range(value+1):
+        # At this point, we are trying to make coins worth v
+        # we have the solutions to 0...V-1
         for c in denominations:
-            # we need to ensure 3 things at this point
-            # first, the coin is not bigger than the value v
-            # second, we have a way to make v-c from our coins
-            # third, the way to make v-c is the minimum
-            # this condition below does that
-            allowed = v - c >= 0 and dp[v - c] != -1 and (dp[v] == -1 or dp[v - c] + 1 <= dp[v])
+            # Now we will check some complexities we assumed earlier 
+            bounds = v - c >= 0
+            can_make = bounds and dp[v - c] != -1
+            # either minimum or first viable combination of coins
+            minimum = can_make and (dp[v - c] + 1 < dp[v] or dp[v] == -1)
 
-            if allowed:
+            if minimum:
                 dp[v] = 1 + dp[v - c]
 
     return dp[value]
-
-print(calculate(24848, [1, 4, 9]))
---
+++ lecture_2.md
@@ -0,0 +1,30 @@
+# Lecture 2 - CMSC351
+
+## Asymptotic time complexity
+
+Note: discarding/ignoring previous knowledge might help for this.
+
+Idea: We have a algorithm which processes an object with "n" length (bit length, length of list, num vertices, etc...)
+Q: How does time change with respect to "n" as $n \rightarrow \infty$?
+
+A specific answer might be: $T(n) = 5n^2 + 6n + 1$, but this is basically impossible to have accurate, too specific
+
+Instead, we use $O(n)$, $\Omega(n)$, and $\theta(n)$
+
+
+**Definition**: $f(n) \in O(g(n))$ when $\exists n_0,C  \forall n >= n_0 f(n) <= C \times g(n)$
+
+Prove from the definition:
+Suppose we want to prove $f(n) \in O(g(n))$
+
+General strategy
+break function down into parts
+
+$f(n) = f_0(n) + f_1(n) + ... + f_m(n)$
+
+if $f_i(n) = D * g(n)$, then C = D, n_0 = 0
+
+otherwise, solve for n in $f_i(n) <= g(n)$
+
+Then we get C = \#functions solved second way + sum of all C from first way
+and we get n_0 to be the maximum n in all the parts solved.
